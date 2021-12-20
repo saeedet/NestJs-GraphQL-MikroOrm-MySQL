282cfee65c96854d084ef2ed61d2afaf84ccdf21
Saeed <saeed_et90@yahoo.com>
2021-12-20 13:20:23 +1100

    graphql added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,20 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BooksModule } from './books/books.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env.local',
     }),
+    GraphQLModule.forRoot({
+      include: [BooksModule],
+      autoSchemaFile: './src/books/graphql/schema.gql',
+    }),
+    BooksModule,
   ],
   controllers: [AppController],
   providers: [AppService],
